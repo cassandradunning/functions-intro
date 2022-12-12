e0512ca6b2f33b5b95e5dbd264917b9cc6ce665d
Cassie Dunning <hello.cassdunning@gmail.com>
2022-12-12 09:54:37 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+// #1: Intro to Functions
+const fillGasTank = (gallons) => {
+    if (gallons < 15) 
+        console.log(`I filled the tank with ${gallons} of gas`)
+    else {
+        console.log (`"You cannot add this much gas to the tank`)
+    }
+    }
+    fillGasTank(16)
+    
